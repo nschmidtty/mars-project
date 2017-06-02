97b5680ef66260e071007da27acf3dea56165c1d
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-02 13:49:12 -0700

    Sorted List

--- src/app/pages/encounters/encounters.component.ts
+++ src/app/pages/encounters/encounters.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Encounter } from '../../models/encounter';
-import { EncountersService } from '../../services/encounters.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Encounter
+} from '../../models/encounter';
+import {
+  EncountersService
+} from '../../services/encounters.service';
 
 @Component({
   selector: 'app-encounters',
@@ -12,16 +19,40 @@ export class EncountersComponent implements OnInit {
 
   encounters: Encounter[] = [];
 
+
   constructor(private encounterService: EncountersService) {
 
   }
 
   ngOnInit() {
     this.encounterService.getData()
-        .subscribe((data) => {
-          this.encounters = data.encounters;
-        })
+      .subscribe((data) => {
+        const re = /\d\d\d\d-\d\d-\d\d\b/g;
+        let encountersFiltered: Encounter[] = [];
+        this.encounters = data.encounters;
+        const sortedAndFiltered = this.encounters.filter((item) => {
+          const re = /\d\d\d\d-\d\d-\d\d\b/g;
+          if (item.date.match(re)) return true;
+          return false;
+        }).sort((a, b) => {
+
+          const x = new Date(a.date);
+          const y = new Date(b.date);
+
+          if (x > y) return -1;
+          if (x < y) return 1;
+          else 0;
+
+        });
+
+        this.encounters = sortedAndFiltered
+
+      });
   }
 }
+
+
+
+
 
 

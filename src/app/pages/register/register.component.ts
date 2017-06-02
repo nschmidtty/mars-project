6076d2662466334e44accc5952e26aa640c6a9c6
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-02 11:07:52 -0700

    Got report form working

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,14 +3,27 @@ import { Job } from '../../models/job';
 import { JobsService } from '../../services/jobs.service';
 import { Colonist } from '../../models/colonist';
 import { ColonistService } from '../../services/colonist.service';
-import { FormGroup, 
-  FormControl, 
-  FormBuilder, 
-  Validators, 
-  ValidatorFn, 
-  AbstractControl }
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  ValidatorFn,
+  AbstractControl
+}
   from '@angular/forms'
 
+const cantBe = (value: string): ValidatorFn => {
+  return (control: AbstractControl) => {
+    return control.value === value ? { 'Cant be this value': value } : null
+  }
+};
+
+const tooOld = (age: string): ValidatorFn => {
+  return (control: AbstractControl) => {
+    return control.value > age ? { 'Too old to go to Mars': age } : null
+  }
+};
 
 @Component({
   selector: 'app-encounters',
@@ -25,33 +38,48 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   NO_JOB_SELECTED = "(none)";
 
-  constructor(private jobService: JobsService, 
-              private colonistService: ColonistService) {  }
+
+  constructor(private jobService: JobsService,
+    private colonistService: ColonistService) { }
 
   ngOnInit() {
     this.jobService.getData()
-        .subscribe((data) => {
-          console.log(data);
-          this.jobs = data.jobs;
-        });
+      .subscribe((data) => {
+        console.log(data);
+        this.jobs = data.jobs;
+      });
 
-        this.registerForm = new FormGroup({
-          name: new FormControl('', [
-          Validators.required, 
-          Validators.maxLength(100), 
-          Validators.minLength(3)
-          ]),
-          age: new FormControl('', [Validators.required]),
-          job_id: new FormControl(this.NO_JOB_SELECTED, [])
-        });
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(100),
+        Validators.minLength(3)
+      ]),
+      age: new FormControl('', [Validators.required, tooOld("60")]),
+      job_id: new FormControl(this.NO_JOB_SELECTED, [cantBe(this.NO_JOB_SELECTED)])
+    });
   }
 
-  postColonist(){
-    const colonist = new Colonist("Billy", "2", "1");
-    this.colonistService.postData(colonist)
-                        .subscribe((newColonist) => {
-                          console.log(newColonist);
-                        });
-  }
+  // postColonist(){
 
+  // }
+
+  register(e) {
+    e.preventDefault();
+    if (this.registerForm.invalid) {
+
+    }
+    else {
+      const name = this.registerForm.get('name').value;
+      const age = this.registerForm.get('age').value;
+      const job_id = this.registerForm.get('job_id').value;
+
+      const colonist = new Colonist(name, age, job_id);
+      this.colonistService.postData(colonist)
+        .subscribe((newColonist) => {
+          console.log(newColonist);
+          window.localStorage.setItem("userID" ,newColonist.colonist.id);
+        });
+    }
+  }
 }
--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,11 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { Aliens } from '../../models/aliens';
-import { AliensService } from '../../services/aliens.service';
-import { Report } from '../../models/report';
-import { ReportService } from '../../services/report.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Aliens
+} from '../../models/aliens';
+import {
+  AliensService
+} from '../../services/aliens.service';
+import {
+  Report
+} from '../../models/report';
+import {
+  ReportService
+} from '../../services/report.service';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  ValidatorFn,
+  AbstractControl
+}
+  from '@angular/forms'
+
+const cantBe = (value: string): ValidatorFn => {
+  return (control: AbstractControl) => {
+    return control.value === value ? {
+      'Cant be this value': value
+    } : null
+  }
+};
+
+const tooOld = (age: string): ValidatorFn => {
+  return (control: AbstractControl) => {
+    return control.value > age ? {
+      'Too old to go to Mars': age
+    } : null
+  }
+};
 
 @Component({
-  selector: 'app-encounters',
+  selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss'],
   providers: [AliensService, ReportService]
@@ -14,26 +50,55 @@ export class ReportComponent implements OnInit {
 
   aliens: Aliens[] = [];
   report: Report;
+  reportForm: FormGroup;
+  NO_ALIEN_SELECTED = "(none)";
 
-  constructor(private alienService: AliensService, 
-              private reportService: ReportService) {
-
+  constructor(private alienService: AliensService,
+    private reportService: ReportService) {
   }
 
   ngOnInit() {
     this.alienService.getData()
-        .subscribe((data) => {
-          console.log(data);
-          this.aliens = data.aliens;
-        })  ;
+      .subscribe((data) => {
+        console.log(data);
+        this.aliens = data.aliens;
+      });
+
+    this.reportForm = new FormGroup({
+      alien_id: new FormControl('', []),
+      description: new FormControl('', []),
+    });
   }
 
-  postAlien(){
-    const report = new Report("Octospider", "2015-10-01", "Web Developer", "4");
-    this.reportService.postData(report)
-                        .subscribe((newReport) => {
-                          console.log(newReport);
-                        });
+  // postAlien() {
+  //   const report = new Report("Octospider", "2015-10-01", "Web Developer", "4");
+  //   this.reportService.postData(report)
+  //     .subscribe((newReport) => {
+  //       console.log(newReport);
+  //     });
+  // }
+
+  reported(e) {
+    e.preventDefault();
+    if (this.reportForm.invalid) {
+
+    } else {
+      const alien = this.reportForm.get('alien_id').value;
+      const description = this.reportForm.get('description').value;
+      const date = this.getFormatedDate()
+      const colonist_id = window.localStorage.userID;
+      
+      const report = new Report(alien, date, description, colonist_id);
+      this.reportService.postData(report)
+        .subscribe((newReport) => {
+          console.log(newReport);
+        });
+    }
+  }
+
+  getFormatedDate(){
+    const date = new Date;
+    return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
   }
 
 }
